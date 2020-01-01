b439221d9432f89fa7ada1ff1966d7c411161606
Alamgir Hossain <alamgir555hossain@gmail.com>
2020-01-01 18:59:25 -0500

    Persist Data and CheckOut cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,10 +6,11 @@ import './cart-dropdown.styles.scss';
 
 import CustomButton from '../custom-button/custom-button.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import CartItem from '../cart-item/cart-item.component';
 
-const CartDropdown = ({cartItems, history}) => (
+const CartDropdown = ({cartItems, history, dispatch }) => (
     <div className="cart-dropdown">
         <div className="cart-items" />
             {
@@ -20,7 +21,10 @@ const CartDropdown = ({cartItems, history}) => (
                     <span className="empty-message">Your cart is empty</span>
                 )
             }
-            <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+            <CustomButton onClick={() => {
+                 history.push('/checkout'); 
+                    dispatch(toggleCartHidden());
+                 }}>GO TO CHECKOUT</CustomButton>
     </div>
 );
 
